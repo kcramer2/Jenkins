245d8a8582fe21a3d08cd14eea14f8c5c2964ad7
kcramer <kjcramer@madisoncollege.edu>
2017-09-27 12:33:43 -0500

    changes made to Docker and Gluster scripts

--- jobGluster.sh
+++ jobGluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /mnt/gluster/chtc/test_open_terminal.mov ./
+cp /mnt/gluster/chtc/submitTesting/test_open_terminal.mov ./
 ./ffmpeg -i test_open_terminal.mov -b:v 400k -s 640x360 test_open_terminal.mp4
 rm test_open_terminal.mov
