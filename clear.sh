811670ea908421484c5e614b64a3222efc9c3636
kcramer <kjcramer@madisoncollege.edu>
2017-10-13 15:31:38 -0500

    removed a.out and modified clear.sh

--- clear.sh
+++ clear.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-rm /home/kcramer/*.out
-rm /home/kcramer/*.err
-rm /home/kcramer/*.log
-rm /home/kcramer/*.error
-rm /home/kcramer/*.metrics
-rm /home/kcramer/*.condor.sub
-rm /home/kcramer/*.mp4
+rm *.out
+rm *.err
+rm *.log
+rm *.error
+rm *.metrics
+rm *.condor.sub
+rm *.mp4
