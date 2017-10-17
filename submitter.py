88e45da7407aa152a7a1749a5ce068305b7f7529
kcramer <kjcramer@madisoncollege.edu>
2017-10-17 14:19:30 -0500

    Changes from testing, including new exception catching and bug fixes for emails. New archive.

--- submitter.py
+++ submitter.py
@@ -1,7 +1,8 @@
 #
 #		Python Submitter File
 #		Kent Cramer II
-#		Edited: 10/13/2017
+#		In collaboration with Emile Tura ( pysendm[basis for mail function], mpi1.sub )
+#		Edited: 10/17/2017
 #
 ############################################################
 #
@@ -13,9 +14,13 @@
 # import OS for shell commands, and time for sleep function
 import os, time, smtplib, subprocess
 
+# Remove old test files
+os.system("./clear.sh")
+
 # creates the base submission commands for regular and dag submissions
 command = "condor_submit"
 command_dag = "condor_submit_dag jobdag.dag"
+error = ''
 
 # Create the list for each of the regular jobs to submit
 tests = ('job1cpu.sub', 'jobDocker.sub', 'jobGluster.sub', 'jobMem.sub', 'mpi_run.sub', 'jobGpu.sub' )
@@ -24,6 +29,7 @@ tests = ('job1cpu.sub', 'jobDocker.sub', 'jobGluster.sub', 'jobMem.sub', 'mpi_ru
 failed_submit = 0
 
 # create email function to call when sending alerts
+# Derived from original pysendm.py By Emile Tura.
 def send_email(error,text):
 	# List of users to notify
 	userList = ["turatsinze","kcramer3"]
@@ -31,8 +37,8 @@ def send_email(error,text):
 	userNames = [u for u in userList]
 
 	# Users email list
-	emailList = [name +  "@wisc.edu" for name in userNames]
-
+	#emailList = [name +  "@wisc.edu" for name in userNames]
+	emailList = ["kcramer3@wisc.edu"]
 
 	# Submit server running the script
 	proc = subprocess.Popen("hostname", shell=True, stdout=subprocess.PIPE, )
@@ -68,12 +74,8 @@ for test in tests:
 	run = command + " " + test
 
 	# Attempts to run the new command variable
-	try:
-		os.system(run)
-
-	# 'handles' the exceptions if the command fails
-	except:
-		# Log output if job submission failed
+	return_val = os.system(run)
+	if return_val != 0:
 		failed = "echo 'job(s) failed to submit:' " + test + " >> submitter.log"
 		os.system(failed)
 
@@ -84,21 +86,29 @@ for test in tests:
 if failed_submit == 1:
 	# Email only once for any non-dag submissions errors. A seperate email template should be utlized
 	text = " One or more the non-dag jobs failed to submit. Please investigate"
-	send_mail(error,text)
+	# opens the log file to pull failure messages
+	log = open('submitter.log', 'r')
+	# adds the text from submitter.log to the error vaariable to pass to send_email function
+	error = log.read()
+	send_email(error,text)
+	log.close()
+	# resets the failure messages
 	failed_submit = 0
 
 # Attempts to pass the command for the DAG job to the command line
-try:
-	os.system(command_dag)
+return_val = os.system(command_dag)
 # 'Handles' errors that occur with the dag submission command
-except:
+if return_val != 0:
         failed_submit = 1
 	failed = "echo 'job failed to submit: jobDAG' >> submitter.log"
         os.system(failed)
 
 if failed_submit == 1:
 	text = " The DAG job submission has failed to submit. Please investigate"
-	send_mail(error,text)
+	log = open('submitter.log', 'r')
+	error = log.read()
+	send_email(error,text)
+	log.close()
 	failed_submit = 0
 
 
@@ -116,6 +126,7 @@ while True:
 	error = list()
 
 	# This set of commands checks for the Jobs in queue. If jobs have not yet been processed, this will not be 0.
+	# Rewritten with collaboration from Emile Tura.
 	print('checking command - iter:' + str(elapsed))
 	status = os.popen("condor_q | grep -i jobs | cut -c 1").read()
 	status = status.rstrip()
@@ -126,7 +137,7 @@ while True:
 		# Begin Checking output files, adds jobs without an out file to the error list.
 		print('if statement 1')
 		print(error)
-		if os.path.isfile("/path/to/cpu1.out") != True:
+		if os.path.isfile("/home/kcramer/cpu1.out") != True:
 			error.append('cpu')
 	        if os.path.isfile("/home/kcramer/gluster.mp4") != True and os.path.isfile("/home/kcramer/gluster.out") != True:
 	                error.append('gluster')
@@ -154,14 +165,16 @@ while True:
 
 			# Outfile(s) not present, writting to log
 			os.system("echo 'Completion of jobs: FAILED. Alerting via Email' >> submitter.log")
-
+			errors = ""
+			for i in error:
+				errors = errors + i + " "  
 			# Sends an email to inform of a failure during the submission checks
-			text = " The following tests have not completed running within 2 hours and may be stuck in the queue" + "\n"
-			send_email(error,test)
+			text = " The following tests have not completed running within 2 hours and may be stuck in the queue: " + "\n"
+			send_email(errors,text)
 			break
 	else:
 		print('failed check')
 	# Increment the time elapsed variable and sleep for 10 minutes before checking again.
 	print(elapsed)
 	elapsed = elapsed + 1
-	time.sleep(6)
+	time.sleep(600)
